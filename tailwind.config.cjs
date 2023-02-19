4aceca5be1b9f27053d8b86d1c47cfb855ad77b3
LifeOfCoding <LifeOfCoding@gmail.com>
2023-02-19 09:42:50 -0500

    Add nvabar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    variants: {
+      extend: {
+        fontWeight: ["responsive", "hover", "focus"],
+        opacity: ["hover"],
+        borderColor: ["hover", "focus"],
+        margin: ["first", "last"],
+        backgroundColor: ["odd", "even"],
+        scale: ["hover", "active", "group-hover"],
+      },
+    },
+    extend: {
+      colors: {
+        "hot-purple": "#2e026d",
+        "hot-pink": "hsl(280,100%,70%)",
+        "transparent-black": "#00000080",
+      },
+    },
   },
   plugins: [],
 };
